c83412cc7141d1b37dfd5423c0ea7e137ec01425
TAM360 <demon6656@gmail.com>
2018-12-05 14:49:02 +0500

    image upload not working properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_053116) do
+ActiveRecord::Schema.define(version: 2018_12_05_093310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2018_12_05_053116) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.string "location"
+    t.integer "ratings"
+    t.integer "diary_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
